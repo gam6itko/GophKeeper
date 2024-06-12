792c4de497c6a37e713289c05f45bd13c5d7f0af
gam6itko <gam6itko@gmail.com>
2024-06-12 15:37:53 +0300

    init2

---
+++ cmd/client/main.go
@@ -0,0 +1,23 @@
+// Клиент должен реализовывать следующую бизнес-логику:
+//	- аутентификация и авторизация пользователей на удалённом сервере;
+//	- доступ к приватным данным по запросу.
+
+package client
+
+//todo -help with version and build date
+
+// register
+// login
+// set master password
+
+// --- with meta information
+// save credentials
+// save text
+// save bin
+// save card
+// save the planet!
+
+// exit
+
+//todo обмен rsa ключами
+// store and sync
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gam6itko/goph-keeper
+
+go 1.21.8
