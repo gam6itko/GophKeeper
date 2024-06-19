f6261976e0677c0121b27ab6b3b24fbaf6bb453f
gam6itko <gam6itko@gmail.com>
2024-06-19 11:47:28 +0300

    123

--- internal/client/server/interfaces.go
+++ internal/client/server/interfaces.go
@@ -4,10 +4,40 @@ import (
 	"context"
 )
 
+type PrivateDataType int
+
+const (
+	TypeLoginPass = iota
+	TypeText
+	TypeBinary
+	ypeBankCard
+)
+
+type LoginDTO struct {
+	Username string
+	Password string
+}
+
+type RegistrationDTO struct {
+	Username string
+	Password string
+}
+
+type PrivateDataDTO struct {
+	datatype PrivateDataType
+	data     []byte
+	meta     map[string]string
+}
+
 type ILoginServer interface {
-	Login(ctx context.Context, username string, password string) error
+	Login(ctx context.Context, dto LoginDTO) error
 }
 
 type IRegistrationServer interface {
-	Register(ctx context.Context, username string, password string) error
+	Register(ctx context.Context, dto RegistrationDTO) error
+}
+
+type IPrivateStorage interface {
+	Load(ctx context.Context, id uint32) (PrivateDataDTO, error)
+	Store(ctx context.Context, dto PrivateDataDTO) error
 }
